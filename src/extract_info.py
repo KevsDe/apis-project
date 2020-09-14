c55c26c0fe82f237444dc76a6f0a19670b9341a3
Kevin Careaga <kevin.careaga.bear@gmail.com>
2020-09-14 12:49:40 +0200

    funciones actualizadas

---
+++ src/extract_info.py
@@ -0,0 +1,34 @@
+import requests
+import pandas as pd 
+
+def importdataset(name):
+    """Import a given dataset"""
+    potter = pd.read_csv(f'Output/{name}.csv',encoding='latin1')
+    return potter
+
+
+def character_selection(character_name='Tom Riddle'):
+    """This function allows you to select available Harry Potter´s character in the potter.csv data set and display the information"""
+    potter = pd.read_csv('Output/potter.csv',encoding='latin1')
+    potter = potter.set_index('Name', drop=False)
+    potter_t = potter.T
+    return potter_t[character_name]
+
+
+def ratio(house_name='Slytherin'):
+    """This function is used to calculate a ratio based in three parameters data frame, column and the attribute of the column, the columna and categoria parameters should be a string"""
+    potter = pd.read_csv('Output/potter.csv',encoding='latin1')
+    potter=potter.query("House != 'Unknown'")
+    a=potter[potter['House']==house_name]
+    b=potter['House']
+
+    p1=len(a)
+    p2=len(b)
+    ratio=(p1/p2)*100
+    return f'The ratio of {house_name} students in Hogwarts School of Witchcraft and Wizardry is {ratio:.2f}'
+
+
+def group(data, columna1, columna2):
+    """Simple group by function, that received three parameters dataframe and two columns"""
+    gp=data.groupby(columna1)[columna2]
+    return gp
